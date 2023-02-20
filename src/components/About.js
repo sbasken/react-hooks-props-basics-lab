cbad50388ac4f00e97f6dd594ecda91bd8c7fe9c
Saki <sbasken@gmail.com>
2023-02-20 07:55:46 -0800

    complete app

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import user from "../data/user"
 
-function About() {
+
+function About(props) {
+  const userBio = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { userBio ? <p>{ props.bio }</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
     </div>
   );
 }
